006bce07556654a6d1de56fee6036f2ce4ab236a
monicasangam <32145733+monicasangam@users.noreply.github.com>
2017-12-11 16:18:24 -0500

    set the model name for the table display

---
+++ collection/AccountsMap.php
@@ -0,0 +1,23 @@
+
+<?php
+namespace collection;
+
+
+ini_set('display_errors', on);
+error_reporting(E_ALL);
+define('DATABASE','ms792' );
+define('USERNAME', 'ms792');
+define('PASSWORD', 'bSzrOJUh');
+define('CONNECTION', 'sql1.njit.edu');
+
+include "collection/Map.php";
+
+class AccountsMap extends Map
+{
+    public function __construct($modelName)
+    {
+        $model = $modelName;
+    }
+}
+
+?>
