ef0110bdd1bcbd374cb5a012e16877a6d2dde3c4
monicasangam <32145733+monicasangam@users.noreply.github.com>
2017-12-13 15:49:36 -0500

    stored the registration values in a session

--- register.php
+++ register.php
@@ -1,36 +1,32 @@
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-  <body>
-  <div class="jumbotron">
-    <form action="register.php" method="get">
-        <label>First Name</label>
-        <input type="text" name=fname value="fname">
-        <br><br>
-        <label>Last Name</label>
-        <input type="text" name=lname value="lname">
-        <br><br>
-        <label>Email</label>
-        <input type="text" name=email value="email">
-        <br><br>
-        <label>Phone</label>
-        <input type="text" name=phone value="phone">
-        <br><br>
-        <label>Birthday</label>
-        <input type="text" name=birthday value="birthday">
-        <br><br>
-        <label>Gender</label>
-        <input type="text" name=gender value="gender">
-        <br><br>
-        <label>Password</label>
-        <input type="password" name="password" value="password">
-        <br><br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
-  </div>
-  </body>
-</html>
+<?php
+
+session_start();
+echo '<h1>You are now registered</h1>';
+
+$fname = $_GET['fname'];
+print("First Name is:" .$fname. "</br>");
+
+$lname = $_GET['lname'];
+print("Last Name is:" .$lname. "</br>");
+
+$email = $_GET['email'];
+print("Email is:" .$email. "</br>");
+
+$phone = $_GET['phone'];
+print("Phone is:" .$phone. "</br>");
+
+$birthday = $_GET['birthday'];
+print("Birthday is:" . $birthday. "</br>");
+
+$gender = $_GET['gender'];
+print("Gender is:" .$gender. "</br>");
+
+
+$password = $_GET['password'];
+$password2 = md5(PASSWORD_BCRYPT);
+print("Password is:" .$password2. "</br>");
+
+header("Location:login.php");
+
+
+?>
