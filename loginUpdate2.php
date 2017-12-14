9ebe315acafed46ad2b1ebca9d442c809b5ec183
monicasangam <32145733+monicasangam@users.noreply.github.com>
2017-12-14 14:18:51 -0500

    added the header and footer to page

---
+++ loginUpdate2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Project</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    <?php
+    include('header.php');
+    ?>
+    <form method="post" action="authenticate.php">
+        <h1>User Login</h1>
+        <label>Username</label>
+        <input type="text" name="username" value="username"></br>
+        <br>
+        <label>Password</label>
+        <input type="password" name="password" value="password"></br>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+    <?php
+    include('footer.php');
+    ?>
+    </body>
+</html>
