086f676c7778ecbebbff5d21fdd96a313cb36968
monicasangam <32145733+monicasangam@users.noreply.github.com>
2017-12-17 15:00:00 -0500

    created a heading for the page

---
+++ header.php
@@ -0,0 +1,10 @@
+<style>
+    .bg-4 {
+        background-color: plum;
+        color: #ffffff;
+    }
+</style>
+
+<header class="container-fluid bg-4 text-center">
+    <h1>Final Project MVC</h1>
+</header>
