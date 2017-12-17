ef7b716724f1c12c3f6ecb137b2896bdf3f6e88a
monicasangam <32145733+monicasangam@users.noreply.github.com>
2017-12-17 15:00:30 -0500

    description of the footer of the page

---
+++ footer.php
@@ -0,0 +1,9 @@
+<style>
+    .bg-4 {
+        background-color: plum;
+        color: #ffffff;
+    }
+</style>
+<footer class="container-fluid bg-4 text-center">
+    <p>IS 601 Final Project</p>
+</footer>
